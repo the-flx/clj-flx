2574be3c1d1ecc405d39b2c4f4c2c37339422047
Jen-Chieh Shen <jcs090218@gmail.com>
2024-12-03 20:42:27 +0800

    update meta data

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-flx "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-flx "0.1.0"
+  :description "Rewrite emacs-flx in Clojure"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns clj-flx.core}
