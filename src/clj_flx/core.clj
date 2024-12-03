fdc6cf4526f143adc3bbe7f031ab20746c36d30e
Jen-Chieh Shen <jcs090218@gmail.com>
2024-12-04 02:27:50 +0800

    learn recur

--- src/clj_flx/core.clj
+++ src/clj_flx/core.clj
@@ -18,6 +18,38 @@
        (word-p char)
        (= (str char) (str/upper-case char))))
 
+(defn boundary-p
+  "Check if LAST-CHAR is the end of a word and CHAR the start of the next.
+
+This function is camel-case aware."
+  [last-char char]
+  (or (not (some? last-char))
+      (and (not (capital-p last-char))
+           (capital-p char))
+      (and (not (word-p last-char))
+           (word-p char))))
+
+(defn inc-vec
+  "Increment each element in VEC between BEG and END by INC.
+
+INC defaults to 1.  BEG defaults to 0 and is inclusive.
+END is not inclusive and defaults to the length of VEC."
+  [vec ^Integer inc ^Integer beg ^Integer end]
+  (let [_inc (or inc 1)
+        _beg (or beg 0)
+        _end (or end (count vec))]
+    (loop [vec vec
+           _count 0
+           _result (list)]
+      (if (empty? vec)
+        _result
+        (recur (rest vec)
+               (+ _count 1)
+               (concat _result (list (if (and (<= _beg _count)
+                                              (< _count _end))
+                                       (+ (first vec) _inc)
+                                       (first vec)))))))))
+
 (defn score
   "Return best score matching QUERY against STR."
   [^String str ^String query]
--- test/clj_flx/core_test.clj
+++ test/clj_flx/core_test.clj
@@ -18,6 +18,10 @@
   (testing "Test capital 2"
     (is (= (capital-p \c) false))))
 
+(deftest inc-vec-test-2
+  (testing "Test inc-vec 1"
+    (is (= (inc-vec (list 1 2 3) 1 0 3) (list 2 3 4)))))
+
 ;; (deftest score-test-1
 ;;   (testing "Score `switch-to-buffer'"
 ;;     (is (= (first (score "switch-to-buffer" "stb")) 237))))
