96f9965ea0690d7e1ac07d0ef1207dafec0ad292
Jen-Chieh Shen <jcs090218@gmail.com>
2024-12-11 22:36:15 +0800

    psh

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-flx "0.1.0"
   :description "Rewrite emacs-flx in Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/the-flx/clj-flx"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
